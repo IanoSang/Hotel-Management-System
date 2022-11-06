4c46980827b5c3b78359b6881b353391c821a188
IanoSang <ianosang18@gmail.com>
2022-11-06 16:02:49 +0300

    feat: urls- add urls paths to room and bookin list and booking form

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from . import views
+from .views import RoomList, BookingList, BookingView
 
+app_name = 'app'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('room_list/', RoomList.as_view(), name='RoomList'),
+    path('booking_list/', BookingList.as_view(), name='BookingList'),
+    path('book/', BookingView.as_view(), name='booking_view')
+
 ]
