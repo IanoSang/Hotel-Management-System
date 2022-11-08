2923f737919a74757d5a0a43fcb942529c45aa57
IanoSang <ianosang18@gmail.com>
2022-11-08 08:19:53 +0300

    feat: urls- adds a path to room category

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import RoomList, BookingList, BookingView
+from .views import RoomList, BookingList, BookingView, RoomDetailView
 
 app_name = 'app'
 urlpatterns = [
     path('room_list/', RoomList.as_view(), name='RoomList'),
     path('booking_list/', BookingList.as_view(), name='BookingList'),
-    path('book/', BookingView.as_view(), name='booking_view')
-
+    path('book/', BookingView.as_view(), name='booking_view'),
+    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),
 ]
