cb3c52839989d9450571673ccf87d464f479025a
IanoSang <ianosang18@gmail.com>
2022-11-12 11:45:02 +0300

    feat: views- adds a room list view listing room categories

--- app/views.py
+++ app/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView, FormView, View
+from django.urls import reverse
 from .models import Room, Booking
 from .forms import AvailabilityForm
 from app.booking_functions.availability import check_availability
 
 
 # Create your views here.
-class RoomList(ListView):
-    model = Room
+def RoomListView(request):
+    room = Room.objects.all()[0]
+    room_categories = dict(room.ROOM_CATEGORIES)
+    room_values = room_categories.values()
+    room_list = []
 
+    for room_category in room_categories:
+        room = room_categories.get(room_category)
+        room_url = reverse('app:RoomDetailView', kwargs={
+                           'category': room_category})
+
+        room_list.append((room, room_url))
+    context = {
+        "room_list": room_list,
+    }
+    return render(request, 'room_list_view.html', context)
 
 class BookingList(ListView):
     model = Booking
