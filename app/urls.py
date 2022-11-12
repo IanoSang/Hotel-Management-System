6abfd927fc40606788ef7c7b2e1fce9529bb9124
IanoSang <ianosang18@gmail.com>
2022-11-12 11:46:09 +0300

    feat: urls- adds a urls to room list

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from .views import RoomList, BookingList, BookingView, RoomDetailView
+from .views import BookingList, BookingView, RoomDetailView, RoomListView
 
 app_name = 'app'
 urlpatterns = [
-    path('room_list/', RoomList.as_view(), name='RoomList'),
+    path('room_list/', RoomListView, name='RoomList'),
     path('booking_list/', BookingList.as_view(), name='BookingList'),
     path('book/', BookingView.as_view(), name='booking_view'),
     path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),
