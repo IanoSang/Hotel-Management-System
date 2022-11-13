5394cd14422288c8966985a4f95dc576b2cabb0a
IanoSang <ianosang18@gmail.com>
2022-11-13 11:17:44 +0300

    urls- removes unnsesary rotes and refactors the routes

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import BookingList, BookingView, RoomDetailView, RoomListView
+from .views import RoomListView, BookingListView, RoomDetailView, CancelBookingView
 
 app_name = 'app'
 urlpatterns = [
-    path('room_list/', RoomListView, name='RoomList'),
-    path('booking_list/', BookingList.as_view(), name='BookingList'),
-    path('book/', BookingView.as_view(), name='booking_view'),
+    path('room_list/', RoomListView, name='RoomListView'),
+    path('booking_list/', BookingListView.as_view(), name='BookingList'),
     path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),
+    path('booking/cancel/<pk>', CancelBookingView.as_view(), name='CancelBookingView'),
 ]
